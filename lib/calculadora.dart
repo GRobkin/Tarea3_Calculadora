f451a52fa0bfe5deb6546863a973e17dbfd0a847
Your Name <your_email@example.com>
2026-05-10 17:07:43 -0300

    nueva interfaz

--- lib/calculadora.dart
+++ lib/calculadora.dart
@@ -15,14 +15,27 @@ class _CalculadoraState extends State<Calculadora> {
   double num1 = 0;
   String operador = "";
 
+
+  void manejarBoton(String valor) {
+    if (valor == "C") {
+      // limpiar
+    } else if (valor == "=" ) {
+      // calcular
+    } else if (valor == "+" || valor == "-" || valor == "*" || valor == "/") {
+      // guardar operador
+    } else {
+      // es un número, mostrarlo en pantalla
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF212121),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
-
             SizedBox(
               height: 80,
               child: TextField(
@@ -30,58 +43,63 @@ class _CalculadoraState extends State<Calculadora> {
                 readOnly: true,
                 textAlign: TextAlign.right,
                 textAlignVertical: TextAlignVertical.center,
-                style: TextStyle(fontSize: 32),
+                style: TextStyle(fontSize: 32, color: Colors.white),
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF2a2f42)),
+                  ),
+                  filled: true,
+                  fillColor: Color(0xFF2f2f2f),
                 ),
               ),
             ),
             SizedBox(height: 16),
             Row(
               children: [
-                Expanded(child:boton(texto: "7") ),
+                Expanded(child:boton(texto: "7", onPressed: () => manejarBoton("7")) ),
                 SizedBox(width: 8),
-                Expanded(child:boton(texto: "8") ),
+                Expanded(child:boton(texto: "8", onPressed: () => manejarBoton("8")) ),
                 SizedBox(width: 8),
-                Expanded(child:boton(texto: "9") ),
+                Expanded(child:boton(texto: "9", onPressed: () => manejarBoton("9")) ),
                 SizedBox(width: 8),
-                Expanded(child:boton(texto: "/") ),
+                Expanded(child:boton(texto: "/", onPressed: () => manejarBoton("/")) ),
               ],
             ),
             SizedBox(height: 20),
             Row(
               children: [
-                Expanded(child:boton(texto: "4") ),
+                Expanded(child:boton(texto: "4", onPressed: () => manejarBoton("4")) ),
                 SizedBox(width: 8),
-                Expanded(child:boton(texto: "5") ),
+                Expanded(child:boton(texto: "5", onPressed: () => manejarBoton("5")) ),
                 SizedBox(width: 8),
-                Expanded(child:boton(texto: "6") ),
+                Expanded(child:boton(texto: "6", onPressed: () => manejarBoton("6")) ),
                 SizedBox(width: 8),
-                Expanded(child:boton(texto: "*") ),
+                Expanded(child:boton(texto: "*", onPressed: () => manejarBoton("*")) ),
               ],
             ),
             SizedBox(height: 20),
             Row(
               children: [
-                Expanded(child:boton(texto: "1") ),
+                Expanded(child:boton(texto: "1", onPressed: () => manejarBoton("1")) ),
                 SizedBox(width: 8),
-                Expanded(child:boton(texto: "2") ),
+                Expanded(child:boton(texto: "2", onPressed: () => manejarBoton("2")) ),
                 SizedBox(width: 8),
-                Expanded(child:boton(texto: "3") ),
+                Expanded(child:boton(texto: "3", onPressed: () => manejarBoton("3")) ),
                 SizedBox(width: 8),
-                Expanded(child:boton(texto: "-") ),
+                Expanded(child:boton(texto: "-", onPressed: () => manejarBoton("-")) ),
               ],
             ),
             SizedBox(height: 20),
             Row(
               children: [
-                Expanded(child:boton(texto: "C") ),
+                Expanded(child:boton(texto: "C", onPressed: () => manejarBoton("C")) ),
                 SizedBox(width: 8),
-                Expanded(child:boton(texto: "0") ),
+                Expanded(child:boton(texto: "0", onPressed: () => manejarBoton("0")) ),
                 SizedBox(width: 8),
-                Expanded(child:boton(texto: "=") ),
+                Expanded(child:boton(texto: "=", onPressed: () => manejarBoton("=")) ),
                 SizedBox(width: 8),
-                Expanded(child:boton(texto: "+") ),
+                Expanded(child:boton(texto: "+", onPressed: () => manejarBoton("+")) ),
               ],
             ),
           ],
--- lib/widgets/boton.dart
+++ lib/widgets/boton.dart
@@ -2,21 +2,27 @@ import 'package:flutter/material.dart';
 
 class boton extends StatelessWidget {
   final String texto;
-
+  final VoidCallback onPressed;
+  
   const boton({
     super.key,
-    required this.texto
+    required this.texto,
+    required this.onPressed
   });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 40,
-      
       child: ElevatedButton(
-        onPressed: () {
-          
-        },
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color(0xFF2f2f2f),
+          foregroundColor: Color(0xFFececec),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ), 
+        ),
         child: Text(
           texto,
           style: TextStyle(fontSize: 24),
